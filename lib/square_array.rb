25e077a443d8e3099933bf52a48de3eb3818a3d9
Isaac Medina <isaac.meedinaa@gmail.com>
2020-03-22 06:16:46 +0000

    Done.

--- lib/square_array.rb
+++ lib/square_array.rb
@@ -1,3 +1,12 @@
 def square_array(array)
-  # your code here
+  final_array = []
+  i = 0
+
+  while i < array.length do
+    new_values = array[i] ** 2
+    final_array.push(new_values)
+    i += 1
+  end
+
+  p final_array
 end
